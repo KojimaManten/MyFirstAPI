f0c8e446ae2a5c793effa95757cbf3c411073d9f
KojimaManten <jumper.manten@icloud.com>
2020-07-19 13:07:29 +0900

    Initial Commit

---
+++ MyFirstAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstAPI
+//
+//  Created by 小島満天 on 2020/07/19.
+//  Copyright © 2020 Manchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
